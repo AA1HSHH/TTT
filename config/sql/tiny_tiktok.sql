b08196b03310eed8b663db6ac5bc8668a59bb3d2
AA1HSHH <974658390@qq.com>
2023-02-10 17:55:25 +0800

    Update tiny_tiktok.sql

--- config/sql/tiny_tiktok.sql
+++ config/sql/tiny_tiktok.sql
@@ -1,36 +1,62 @@
 CREATE TABLE `t_user` (
   `id` bigint PRIMARY KEY NOT NULL AUTO_INCREMENT,
-  `name` varchar(32) NOT NULL,
-  `passwd` varchar(32) NOT NULL
-);
-
-CREATE TABLE `t_video` (
-  `id` int PRIMARY KEY NOT NULL AUTO_INCREMENT,
-  `owner_id` int,
-  `title` varchar(255),
-  `play_url` varchar(255),
-  `cover_url` varchar(255)
-);
-
-CREATE TABLE `t_video_comment` (
-  `id` int PRIMARY KEY NOT NULL AUTO_INCREMENT,
-  `video_id` int,
-  `writer_id` int,
-  `cotent` varchar(255)
+  `name` varchar(32) UNIQUE NOT NULL,
+  `passwd` varchar(32) NOT NULL,
+  `follow_count` bigint NOT NULL DEFAULT 0,
+  `follower_count` bigint NOT NULL DEFAULT 0
 );
 
 CREATE TABLE `t_follow` (
-  `id` int PRIMARY KEY NOT NULL AUTO_INCREMENT,
-  `user_id` int,
-  `fans_id` int
+  `user_id` bigint,
+  `fans_id` bigint,
+  PRIMARY KEY (`user_id`, `fans_id`)
 );
 
-ALTER TABLE `t_video` ADD FOREIGN KEY (`owner_id`) REFERENCES `t_user` (`id`);
+CREATE TABLE `t_video` (
+  `id` bigint PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  `author_id` bigint,
+  `title` varchar(255) NOT NULL,
+  `publish_time` datetime NOT NULL,
+  `play_url` varchar(255) NOT NULL,
+  `cover_url` varchar(255) NOT NULL,
+  `favorite_count` bigint NOT NULL DEFAULT 0,
+  `comment_count` bigint NOT NULL DEFAULT 0
+);
+
+CREATE TABLE `t_video_comment` (
+  `id` bigint PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  `video_id` bigint,
+  `writer_id` bigint,
+  `content` varchar(255),
+  `create_date` datetime,
+  `is_delete` datetime
+);
+
+CREATE TABLE `t_video_favorite` (
+  `id` bigint PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  `video_id` bigint,
+  `liker_id` bigint,
+  `is_delete` datetime
+);
+
+CREATE INDEX `t_user_index_0` ON `t_user` (`name`);
+
+CREATE INDEX `t_video_comment_index_1` ON `t_video_comment` (`video_id`);
+
+CREATE INDEX `t_video_favorite_index_2` ON `t_video_favorite` (`video_id`);
+
+CREATE INDEX `t_video_favorite_index_3` ON `t_video_favorite` (`liker_id`);
+
+ALTER TABLE `t_follow` ADD FOREIGN KEY (`user_id`) REFERENCES `t_user` (`id`);
+
+ALTER TABLE `t_follow` ADD FOREIGN KEY (`fans_id`) REFERENCES `t_user` (`id`);
+
+ALTER TABLE `t_video` ADD FOREIGN KEY (`author_id`) REFERENCES `t_user` (`id`);
 
 ALTER TABLE `t_video_comment` ADD FOREIGN KEY (`video_id`) REFERENCES `t_video` (`id`);
 
 ALTER TABLE `t_video_comment` ADD FOREIGN KEY (`writer_id`) REFERENCES `t_user` (`id`);
 
-ALTER TABLE `t_follow` ADD FOREIGN KEY (`user_id`) REFERENCES `t_user` (`id`);
+ALTER TABLE `t_video_favorite` ADD FOREIGN KEY (`video_id`) REFERENCES `t_video` (`id`);
 
-ALTER TABLE `t_follow` ADD FOREIGN KEY (`fans_id`) REFERENCES `t_user` (`id`);
+ALTER TABLE `t_video_favorite` ADD FOREIGN KEY (`liker_id`) REFERENCES `t_user` (`id`);
