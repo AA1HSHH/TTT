af5cee305c44b4f6b56e6906ea8499b584d9bb47
AA1HSHH <974658390@qq.com>
2023-02-25 17:39:38 +0800

    undate sql v2

--- config/sql/tiny_tiktok.sql
+++ config/sql/tiny_tiktok.sql
@@ -79,9 +79,9 @@ ALTER TABLE `t_video_favorite` ADD FOREIGN KEY (`video_id`) REFERENCES `t_video`
 
 ALTER TABLE `t_video_favorite` ADD FOREIGN KEY (`liker_id`) REFERENCES `t_user` (`id`);
 
-INSERT INTO `t_user`(`id`,`name`,`passwd`,`avatar`,`work_count`,`background_image`,`signature`) VALUES(1,'DOU1','$2a$14$VSYjOFVVrZIiKhH3QKBXL.PjXQ2DqCj.i9Otzdpe.tLqNcaCTI49e','https://img-s-msn-com.akamaized.net/tenant/amp/entityid/AAOEiFs.img',2,'https://img-s-msn-com.akamaized.net/tenant/amp/entityid/AAOEiFs.img','Carpe Diem.');
-INSERT INTO `t_user`(`id`,`name`,`passwd`,`avatar`) VALUES(2,'DOU2','$2a$14$SxKboOBj8/yUlIFnrpkADuEyGMW0h/yuRoWe9mP.4hkb7.0FSptMW','https://img-s-msn-com.akamaized.net/tenant/amp/entityid/AAOEa3q.img');
-INSERT INTO `t_user`(`id`,`name`,`passwd`,`avatar`) VALUES(3,'DOU3','$2a$14$uksl0e5kiYo5ZPxAabxhQOOpOoK9VoHOI2qM8eoiqRSptDLdDUjPi','https://img-s-msn-com.akamaized.net/tenant/amp/entityid/AAOEhRJ.img');
+INSERT INTO `t_user`(`id`,`name`,`passwd`,`avatar`,`work_count`,`background_image`,`signature`,`follow_count`,`follower_count`) VALUES(1,'DOU1','$2a$14$VSYjOFVVrZIiKhH3QKBXL.PjXQ2DqCj.i9Otzdpe.tLqNcaCTI49e','https://img-s-msn-com.akamaized.net/tenant/amp/entityid/AAOEiFs.img',2,'https://img-s-msn-com.akamaized.net/tenant/amp/entityid/AAOEiFs.img','Carpe Diem.',2,2);
+INSERT INTO `t_user`(`id`,`name`,`passwd`,`avatar`,`follow_count`,`follower_count`) VALUES(2,'DOU2','$2a$14$SxKboOBj8/yUlIFnrpkADuEyGMW0h/yuRoWe9mP.4hkb7.0FSptMW','https://img-s-msn-com.akamaized.net/tenant/amp/entityid/AAOEa3q.img',1,1);
+INSERT INTO `t_user`(`id`,`name`,`passwd`,`avatar`,`follow_count`,`follower_count`) VALUES(3,'DOU3','$2a$14$uksl0e5kiYo5ZPxAabxhQOOpOoK9VoHOI2qM8eoiqRSptDLdDUjPi','https://img-s-msn-com.akamaized.net/tenant/amp/entityid/AAOEhRJ.img',1,1);
 INSERT INTO `t_user`(`id`,`name`,`passwd`,`avatar`) VALUES(4,'DOU4','$2a$14$mdh8SU4HjC9Lm8FfLVDdiuygKAO92Tjm41YwXK9wxA8FO9ngPrBby','https://img-s-msn-com.akamaized.net/tenant/amp/entityid/AAOEcdP.img');
 INSERT INTO `t_user`(`id`,`name`,`passwd`,`avatar`) VALUES(5,'DOU5','$2a$14$yirD4Rubtz3hlxf2/pS6bOiGaqqDBx7be6JFqAZpaOr0FWWhwqnpi','https://img-s-msn-com.akamaized.net/tenant/amp/entityid/AAOEhRI.img');
 
@@ -90,5 +90,9 @@ INSERT INTO `t_video`(`id`,`author_id`,`title`,`publish_time`,`play_url`,`cover_
 INSERT INTO `t_video`(`id`,`author_id`,`title`,`publish_time`,`play_url`,`cover_url`) VALUES (3,3,'DOU3 mountain','2023-02-11 10:09:17','https://prod-streaming-video-msn-com.akamaized.net/0b927d99-e38a-4f51-8d1a-598fd4d6ee97/3493c85c-f35a-488f-9a8f-633e747fb141.mp4','https://img-s-msn-com.akamaized.net/tenant/amp/entityid/AAOEhRG.img');
 INSERT INTO `t_video`(`id`,`author_id`,`title`,`publish_time`,`play_url`,`cover_url`) VALUES (4,5,'DOU5 farmland','2023-02-11 10:12:17','https://prod-streaming-video-msn-com.akamaized.net/e6f8c1b2-b1ac-4343-a8bf-bd79a4d25380/9de24622-e13e-4741-95a6-04fdc39eb2b0.mp4','https://img-s-msn-com.akamaized.net/tenant/amp/entityid/AAOEhXX.img');
 
-INSERT INTO `t_follow`(`user_id`, `fans_id`) VALUES (1,2)
-INSERT INTO `t_follow`(`user_id`, `fans_id`) VALUES (1,2)
+INSERT INTO `t_follow`(`user_id`, `fans_id`) VALUES (1,2);
+INSERT INTO `t_follow`(`user_id`, `fans_id`) VALUES (2,1);
+INSERT INTO `t_follow`(`user_id`, `fans_id`) VALUES (1,3);
+INSERT INTO `t_follow`(`user_id`, `fans_id`) VALUES (3,1);
+INSERT INTO `t_message`(`id`,`from`,`to`,`content`,`create_time`) VALUES (1,2,1,'hello',1677316912576);
+INSERT INTO `t_message`(`id`,`from`,`to`,`content`,`create_time`) VALUES (2,3,1,'hi',1677316912572);
